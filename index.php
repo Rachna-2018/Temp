f1c6e32b5564955606216cb281737ef9d2653a70
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-09-18 12:15:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,17 +9,16 @@ if($method == 'POST')
 	$com = strtolower($com);
 	
 		
-	//if ($com == 'listcity' or $com == 'liststates' or $com == 'amountsold' or $com == 'margin' or $com == 'qtysold' or $com=='shoplist' or $com=='listfamily' or $com=='listcategory' or $com=='listarticle') 
-	//{
+	
 		if(isset($json->queryResult->parameters->STATE))
 		{	$STATE= $json->queryResult->parameters->STATE; } else {$STATE = '0';}
-		$STATE= strtoupper($STATE);
+		
 		if(isset($json->queryResult->parameters->CITY))
 		{	$CITY= $json->queryResult->parameters->CITY; } else {$CITY = '0';}
-		//$CITY= $json->queryResult->parameters->CITY;
-		$CITY= strtoupper($CITY);
+				
 		if(isset($json->queryResult->parameters->SHOPNAME))
 		{	$SHOPNAME= $json->queryResult->parameters->SHOPNAME; } else {$SHOPNAME = '0';}
+	
 		if(isset($json->queryResult->parameters->YR))
 		{	$YR= $json->queryResult->parameters->YR; } else {$YR = '0';}
 		
@@ -27,36 +26,44 @@ if($method == 'POST')
 		{	$QTR= $json->queryResult->parameters->QTR; } else {$QTR = '0';}
 		
 		if(isset($json->queryResult->parameters->MTH))
-		{	$MTH= $json->queryResult->parameters->MTH; } else {$MTH = '0';}
-	
-	
-	     if(isset($json->queryResult->parameters->FAMILY))
+		{	$MTH= $json->queryResult->parameters->MTH; } else {$MTH = '0';}	
+
+	   	if(isset($json->queryResult->parameters->FAMILY))
 		{	$FAMILY= $json->queryResult->parameters->FAMILY; } else {$FAMILY = '0';}
 	
-	     if(isset($json->queryResult->parameters->CATEGORY))
+	     	if(isset($json->queryResult->parameters->CATEGORY))
 		{	$CATEGORY= $json->queryResult->parameters->CATEGORY; } else {$CATEGORY = '0';}
 	
-	      if(isset($json->queryResult->parameters->ARTICLE))
+	     	if(isset($json->queryResult->parameters->ARTICLE))
 		{	$ARTICLE= $json->queryResult->parameters->ARTICLE; } else {$ARTICLE = '0';}
-		//$SHOPNAME= $json->queryResult->parameters->SHOPNAME;
+	
+		if(isset($json->queryResult->parameters->my-action))
+		{	$my_previous_action = $json->queryResult->parameters->my-action; } else {$my_previous_action = "";}
+		
+		if(isset($json->queryResult->action))
+		{	$my_action = $json->queryResult->action; } else {$my_action = "";}
+		
+		if($my_action == 'PreviousContext'){$my_action = $my_previous_action;}
+	
+		$CITY= strtoupper($CITY);
+		$STATE= strtoupper($STATE);
 		$SHOPNAME= strtoupper($SHOPNAME);
+		$FAMILY= strtoupper($FAMILY);
+		$CATEGORY= strtoupper($CATEGORY);
+		$ARTICLE= strtoupper($ARTICLE);
 		$YR= strtoupper($YR);
 		$MTH= strtoupper($MTH);
 		$QTR= strtoupper($QTR);
+	
 		$SHOPNAME = str_replace(' ', '', $SHOPNAME);
 		$CITY = str_replace(' ', '', $CITY);
 		$STATE = str_replace(' ', '', $STATE);
+		$FAMILY = str_replace(' ', '', $FAMILY);
+		$CATEGORY = str_replace(' ', '', $CATEGORY);
+		$ARTICLE = str_replace(' ', '', $ARTICLE);
 		$YR = str_replace(' ', '', $YR);
 		$MTH = str_replace(' ', '', $MTH);
 		$QTR = str_replace(' ', '', $QTR);
-		
-		$FAMILY= strtoupper($FAMILY);
-		$FAMILY = str_replace(' ', '', $FAMILY);
-		$CATEGORY= strtoupper($CATEGORY);
-		$CATEGORY = str_replace(' ', '', $CATEGORY);
-		$ARTICLE= strtoupper($ARTICLE);
-		$ARTICLE = str_replace(' ', '', $ARTICLE);
-		
 		
 		if($CITY=="" ){	$CITY='0';}
 		if($STATE=="" ){$STATE='0';}
@@ -66,6 +73,7 @@ if($method == 'POST')
 		if (in_array($YR, $userespnose)) {$YR='0';}
 		if (in_array($QTR, $userespnose)) {$QTR='0';}
 		if (in_array($MTH, $userespnose)) {$MTH='0';}
+	
 		$useres = array("PLEASEIGNORE", "IGNORE","IGNOREIT");
 		if (in_array($STATE, $useres)) {$STATE='0';}
 		if (in_array($CITY, $useres)) {$CITY='0';}
@@ -74,53 +82,45 @@ if($method == 'POST')
 		if (in_array($CATEGORY, $useres)) {$CATEGORY='0';}
 		if (in_array($ARTICLE, $useres)) {$ARTICLE='0';}    
 		    
-		
-	//	echo $MTH;
-	//	echo $QTR;
-		    
-		
-		
-		
-		
 		$userespnose = array("EACH", "EVERY","ALL");
 		if(in_array($STATE, $userespnose))
 		{
 			$STATE = 'ALL';
 		}
-		$userespnose = array("EACH", "EVERY","ALL");
+	
 		if(in_array($SHOPNAME, $userespnose))
 		{
 			$SHOPNAME = 'ALL';
 		}
-		$userespnose = array("EACH", "EVERY","ALL");
+		
 		if(in_array($CITY, $userespnose))
 		{
 			$CITY = 'ALL';
 		}
-		$userespnose = array("EACH", "EVERY","ALL");
+		
 		if(in_array($YR, $userespnose))
 		{
 			$YR = 'ALL';
 		}
-			$userespnose = array("EACH", "EVERY","ALL");
+		
 		if(in_array($FAMILY, $userespnose))
 		{
 			$FAMILY = 'ALL';
 		}
 		
-			$userespnose = array("EACH", "EVERY","ALL");
+		
 		if(in_array($CATEGORY, $userespnose))
 		{
 			$CATEGORY = 'ALL';
 		}
 		
-			$userespnose = array("EACH", "EVERY","ALL");
+		
 		if(in_array($ARTICLE, $userespnose))
 		{
 			$ARTICLE = 'ALL';
 		}
 		$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_DEV.xsjs?command=$com&STATE=$STATE&CITY=$CITY&SHOPNAME=$SHOPNAME&YR=$YR&QTR=$QTR&MTH=$MTH&FAMILY=$FAMILY&CATEGORY=$CATEGORY&ARTICLE=$ARTICLE";		
-		//echo $json_url;
+		
 		$username    = "SANYAM_K";
     		$password    = "Welcome@123";
 		$ch      = curl_init( $json_url );
@@ -230,7 +230,7 @@ if($method == 'POST')
 			$speech .= "Which would you prefer?";
 			
 		}
-	else if ($com == 'listyear')
+		else if ($com == 'listyear')
 		{
 			$speech = "You can see values for following years";
 			$speech .= "\r\n";
@@ -245,9 +245,7 @@ if($method == 'POST')
 		}
 		
 			
-	//}
 	
-	//echo $FAMILY;
 	$response = new \stdClass();
     	$response->fulfillmentText = $speech;
     	$response->source = "webhook";
